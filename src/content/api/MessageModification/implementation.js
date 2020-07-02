f0a533665081979b79a647b4582ed84909dc187a
John Bieling <john.bieling@gmx.de>
2020-07-03 00:38:48 +0200

    WIP to MailExtension with API candidate

--- src/content/api/MessageModification/implementation.js
+++ src/content/api/MessageModification/implementation.js
@@ -2,7 +2,10 @@
 
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-  
+var { setTimeout, clearTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm");
+// inculding EnigmailStdlib because it includes msgHdrsDelete
+var { EnigmailStdlib } = ChromeUtils.import( "chrome://openpgp/content/modules/stdlib.jsm");
+
 var MessageModification = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {    
     context.callOnClose(this);
@@ -10,6 +13,7 @@ var MessageModification = class extends ExtensionCommon.ExtensionAPI {
     return {
       MessageModification: {
         
+
         setSubjectOfMessage: async function(aID, aSubject) {
           let msgHdr = context.extension.messageManager.get(aID);
           if (msgHdr) {
@@ -19,38 +23,74 @@ var MessageModification = class extends ExtensionCommon.ExtensionAPI {
           return false;
         },
         
-        getMessageServerType: async function(aID) {
+        selectMessage: async function(aID) {
+          const msgHdr = context.extension.messageManager.get(aID);
+          const win = Services.wm.getMostRecentWindow("mail:3pane");
+          win.gFolderTreeView.selectFolder(msgHdr.folder, true);
+          win.gFolderDisplay.selectMessage(msgHdr);
+        },
+
+        getMessageFlags: async function(aID) {
           let msgHdr = context.extension.messageManager.get(aID);
           if (msgHdr) {
-            return msgHdr.folder.server.type;
+            return msgHdr.flags;
+          }
+          return false;          
+        },
+
+        getFolderServerType: async function(aMailFolder) {
+          //https://searchfox.org/comm-central/source/mail/components/extensions/parent/ext-messages.js#115
+          let folder = context.extension.folderManager.get(aMailFolder.accountId, aMailFolder.path);
+          if (folder) {
+            return folder.server.type;
           }
           return false;          
         },
         
-        updateMessage: async function(aID, aContent) {
-          // CopyFileMessage uses a callback -> Promisify
+        addRaw: async function(aContent, aMailFolder, aID) {
           return new Promise(function(resolve, reject) {
-            let msgHdr = context.extension.messageManager.get(aID);
-            
-            let copyListener = {
+              let folder = context.extension.folderManager.get(aMailFolder.accountId, aMailFolder.path);
+
+              //to be replaced
+              let msgHdr = context.extension.messageManager.get(aID);
+
+              let postActionInfo = {};              
+              let postActions = function() {
+                  let msgHeader = folder.GetMessageHeader(postActionInfo.key);
+                  let newId = context.extension.messageManager.convert(msgHeader).id;
+                  if (msgHeader.flags & 2) folder.addMessageDispositionState(msgHeader,0);
+                  if (msgHeader.flags & 4096) folder.addMessageDispositionState(msgHeader,1);                  
+                  return newId;
+                  //Delete old mail
+                  //let list = Components.classes["@mozilla.org/array;1"].createInstance(Components.interfaces.nsIMutableArray);
+                  //list.appendElement(msgHdr, false);
+                  //folder.deleteMessages(list,null,true,true,null,false);
+                  //we delete using the
+              };
+
+              let copyListener = {
                 QueryInterface : ChromeUtils.generateQI(["nsIMsgCopyServiceListener"]),
                 GetMessageId: function (messageId) {},
                 OnProgress: function (progress, progressMax) {},
-                OnStartCopy: function () {},
-                OnStopCopy: function (status) {
-                  resolve(true);
-                },
                 SetMessageKey: function (key) {
-                    /*if (msgHdr.folder.server.type == "imap" || msgHdr.folder.server.type == "news") {
-                        Components.classes["@mozilla.org/messenger/services/session;1"].getService(Components.interfaces.nsIMsgMailSession).AddFolderListener(objEditemailsubject.folderListener, Components.interfaces.nsIFolderListener.all);
-                        objEditemailsubject.folderListener.key = key;
-                        objEditemailsubject.folderListener.URI = objEditemailsubject.msgFolder.URI;
+                  console.log("EES: SetMessageKey");
+                  postActionInfo.key = key;
+                  postActionInfo.URI = folder.URI;
+                }, 
+                OnStartCopy: function () {},
+                OnStopCopy: function (statusCode) {
+                    if (statusCode === 0) {
+                      console.log("EES: Copy complete");
+                      //IMAP and NEWS needs time and we should listen for the folder...
+                      resolve(postActions());
+                    } else {
+                      console.log("EES: Error copying message: " + statusCode);
+                      resolve(false);
                     }
-                    else setTimeout(function() {objEditemailsubject.postActions(key);}, 500);*/
-                } 
+                }
             }
             
-            if (msgHdr) {
+            if (folder) {
               // openPGP is using similar code, maybe a bit more modern
               var tempFile = Services.dirsvc.get("TmpD", Ci.nsIFile);
               tempFile.append("EMS.eml");
@@ -60,9 +100,6 @@ var MessageModification = class extends ExtensionCommon.ExtensionAPI {
               foStream.init(tempFile, 2, 0x200, false); // open as "write only"
               foStream.write(aContent, aContent.length);
               foStream.close();
-                      
-              //objEditemailsubject.list = Components.classes["@mozilla.org/array;1"].createInstance(Components.interfaces.nsIMutableArray);
-              //objEditemailsubject.list.appendElement(objEditemailsubject.msgHeader, false);
 
               var fileSpec = Components.classes["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
               fileSpec.initWithPath(tempFile.path);
@@ -70,9 +107,9 @@ var MessageModification = class extends ExtensionCommon.ExtensionAPI {
               extService.deleteTemporaryFileOnExit(fileSpec);
 
               var copyMess = Components.classes["@mozilla.org/messenger/messagecopyservice;1"].getService(Components.interfaces.nsIMsgCopyService);
-              copyMess.CopyFileMessage(fileSpec, msgHdr.folder, null, false, msgHdr.flags, msgHdr.getStringProperty("keywords"), copyListener, null /*msgWindow*/);
+              copyMess.CopyFileMessage(fileSpec, folder, null, false, msgHdr.flags, msgHdr.getStringProperty("keywords"), copyListener, null /*msgWindow*/);
             } else {
-              resolve(null);
+              resolve(false);
             }
           });
         }
--- src/content/editemailsubject.js
+++ src/content/editemailsubject.js
@@ -18,6 +18,43 @@
 
 var editEmailSubjectMain = {
 
+	// open edit popup
+	edit: async function (info) {
+		this.msg = {};
+		this.msg.localMode = await editEmailSubjectPreferences.getPrefValue("localOnly");
+
+		if (info.selectedMessages && info.selectedMessages.messages.length > 0) {
+			let MessageHeader = info.selectedMessages.messages[0];
+			this.msg.folder = MessageHeader.folder;
+			this.msg.subject = MessageHeader.subject;
+			this.msg.date = MessageHeader.date;
+			this.msg.id = MessageHeader.id;
+			this.msg.alreadyModified = false;
+
+			let flags = await messenger.MessageModification.getMessageFlags(this.msg.id);
+			//It looks like TB is storing a leading Re: not as part of the subject, but inside a flag, which is not honored by the subject member
+			if (flags & 0x0010) this.msg.subject = "Re: " + this.msg.subject;
+			
+			// in remoteMode, if the header contains X-EditEmailSubject, we show a warning about being already modified
+			if (!this.msg.localMode) {
+				let full = await messenger.messages.getFull(this.msg.id);
+				this.msg.headers = full.headers;
+				this.msg.alreadyModified = this.msg.headers.hasOwnProperty("x-editemailsubject");				
+				this.msg.raw = await messenger.messages.getRaw(this.msg.id);
+			}
+
+			messenger.runtime.onMessage.addListener(this.handleMessage);	
+			this.msg.popupWindow = await messenger.windows.create({
+				height: this.msg.alreadyModified ? 260 : 170,
+				width: 500,
+				url: "/content/editemailsubjectPopup.html",
+				type: "popup"
+			});
+		} else {
+			console.log("No Message Selected!");
+		}
+	},
+
 	// communication with popup window
 	// this is called within global context
 	handleMessage: function(request, sender, sendResponse) {
@@ -27,23 +64,30 @@ var editEmailSubjectMain = {
 					sendResponse(editEmailSubjectMain.msg);
 				break;
 				case "requestUpdate":
-					if (editEmailSubjectMain.msg.localMode) {
-						editEmailSubjectMain.modifySubject(request);
-					} else {
-						editEmailSubjectMain.modifyRawMessage(request);
+					if (request.newSubject != this.msg.subject) {
+						if (editEmailSubjectMain.msg.localMode) {
+							// just update the subject value in the Thunderbird DB, do not change the actual email
+							editEmailSubjectMain.updateSubject(request);
+						} else {
+							//change the entire email 
+							editEmailSubjectMain.updateMessage(request);
+						}
 					}
 				break;
 			}
 		}
 	},
 	
-	modifySubject: function(request){
-		// update the subject in Thunderbirds message database
+	
+	
+	// just update the subject value in the Thunderbird DB, do not change the actual email
+	// it would be nice to be able to do this via messages.update(), but the newProperties obj does not have a subject member.
+	updateSubject: function(request){
 		messenger.MessageModification.setSubjectOfMessage(editEmailSubjectMain.msg.id, request.newSubject);
 	},
 	
-	modifyRawMessage: async function(request) {
-		
+	//change the entire email (add new + delete original)
+	updateMessage: async function(request) {
 		let raw = this.msg.raw
 						.replace(/\r/g, "") //for RFC2822
 						.replace(/\n/g, "\r\n");
@@ -54,15 +98,16 @@ var editEmailSubjectMain = {
 		let headers =raw.substring(0, headerEnd+2).replace(/\r\r/,"\r");
 		let body = raw.substring(headerEnd+2);
 		
-		// update subject
+		// update subject, check if subject is multiline
+		while(headers.match(/\r\nSubject: .*\r\n\s+/))
+			headers = headers.replace(/(\r\nSubject: .*)(\r\n\s+)/, "$1 ");
 		headers = headers.replace(/\nSubject: .*\r\n/, "\nSubject: " + unescape(encodeURIComponent(request.newSubject)) + "\r\n");
 
 		// Some IMAP provider (for ex. GMAIL) doesn't register changes in source if the main headers
 		// are not different from an existing message. To work around this limit, the "Date" field is 
 		// modified, if necessary, adding a second to the time (or decreasing a second if second are 59)	
-		if (await messenger.MessageModification.getMessageServerType(this.msg.id) == "imap") {
+		if (await messenger.MessageModification.getFolderServerType(this.msg.folder) == "imap") {
 			try {
-				console.log(this.msg.headers["date"][0]);
 				let date = this.msg.headers["date"][0].replace(/(\d{2}):(\d{2}):(\d{2})/, function (str, p1, p2, p3) {
 					var seconds = parseInt(p3) + 1; 
 					if (seconds > 59) seconds = 58;
@@ -77,12 +122,10 @@ var editEmailSubjectMain = {
 			}
 		}
 		
-		// why?
-		//headers = headers.replace(/^From - .+\r\n/, "");
-		//headers = headers.replace(/X-Mozilla-Status.+\r\n/, "");
-		//headers = headers.replace(/X-Mozilla-Status2.+\r\n/, "");
-		//headers = headers.replace(/X-Mozilla-Keys.+\r\n/, "");
-
+		// HACK: without changing the message-id, the MessageHeader obj of the new message and the old message will
+		//share the same ID. It seems this was not the case in TB68.
+		headers = headers.replace(/\nMessage-ID: *.*\r\n/, "\nMessage-ID: " + this.msg.headers["message-id"] + ".1\r\n");		
+		
 		// update or modify X-EditEmailSubject headers
 		let now = new Date;
 		let EditEmailSubjectHead = ("X-EditEmailSubject: " + now.toString()).replace(/\(.+\)/, "").substring(0,75);
@@ -93,43 +136,11 @@ var editEmailSubjectMain = {
 			headers = headers.replace(/\nX-EditEmailSubject: .+\r\n/,"\n" + EditEmailSubjectHead + "\r\n");
 		}
 		
-		await messenger.MessageModification.updateMessage(this.msg.id, headers + body);	
-
-		console.log("OLD  ==============================================================");
-		console.log(this.msg.raw);
-		console.log("NEW  ==============================================================");
-		console.log(headers + body);				
-	},
-
-	edit: async function (info) {
-		this.msg = {};
-		this.msg.localMode = await editEmailSubjectPreferences.getPrefValue("localOnly");
-
-		if (info.selectedMessages && info.selectedMessages.messages.length > 0) {
-			let MessageHeader = info.selectedMessages.messages[0];
-			this.msg.subject = MessageHeader.subject;
-			this.msg.date = MessageHeader.date;
-			this.msg.id = MessageHeader.id;
-			this.msg.alreadyModified = false;
-
-			// in remoteMode, if the header contains X-EditEmailSubject, we show a warning about being already modified
-			if (!this.msg.localMode) {
-				let full = await messenger.messages.getFull(this.msg.id);
-				this.msg.headers = full.headers;
-				this.msg.alreadyModified = this.msg.headers.hasOwnProperty("x-editemailsubject");				
-				this.msg.raw = await messenger.messages.getRaw(this.msg.id);
-			}
-
-
-			messenger.runtime.onMessage.addListener(this.handleMessage);	
-			this.msg.popupWindow = await messenger.windows.create({
-				height: this.msg.alreadyModified ? 260 : 170,
-				width: 500,
-				url: "/content/editemailsubjectPopup.html",
-				type: "popup"
-			});
-		} else {
-			console.log("No Message Selected!");
+		let newID = await messenger.MessageModification.addRaw(headers + body, this.msg.folder, this.msg.id);	
+		if (newID) {
+			console.log("Success [" + this.msg.id + " vs " + newID + "]");
+			await messenger.messages.delete([this.msg.id], true);
+			//await messenger.MessageModification.selectMessage(newID);
 		}
 	}
 };
